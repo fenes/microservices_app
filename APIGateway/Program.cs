b7c61f91d5678b05b0fff70daa886e17850eaecf
fenes <fenesguzel@gmail.com>
2024-06-30 17:42:55 +0300

    runs on ocelot debug.json

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,13 +1,24 @@
+using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+  .AddJsonFile("ocelot.json")
+  .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true)
+  .AddEnvironmentVariables();
+
 builder.Services.AddOcelot();
-
 var app = builder.Build();
-
-app.UseOcelot().Wait();
+app.Use(async (context, next) =>
+{
+  Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+  await next();
+  Console.WriteLine($"Response Status Code: {context.Response.StatusCode}");
+});
+await app.UseOcelot();
 
 app.Run();
